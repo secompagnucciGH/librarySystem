86d3ffe129d5c73e45773c3fca330d9b319b9ff5
Sebastian <jaguar@DESKTOP-7BL6E41>
2024-06-11 13:32:47 -0300

    change- Lombook anotations, styles

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -3,26 +3,25 @@ package biblioteca;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
 
 @Data
-@ToString
 @AllArgsConstructor
 public class Libro {
-    private long ID;
+
+    private int ID;
     private String titulo;
     private String autor;
-    /**
-     * en categoria deberíamos agregar una lista para cada categoría,y que cada categoria entre dentro de un <ENUM>
-     */
+    // en categoria deberíamos agregar una lista para cada categoría,y que cada categoria entre dentro de un <ENUM>
     private String categoria;
     private String ISBN;
     private Double precio;
     private int stock;
 
-    @Override
-    public String toString() {
-        long var10000 = this.getID();
-        return "Libro(ID=" + var10000 + ", titulo=" + this.getTitulo() + ", autor=" + this.getAutor() + ", categoria=" + this.getCategoria() + ", ISBN=" + this.getISBN() + ", precio= $ " + this.getPrecio() + ", stock=" + this.getStock() + ")";
-    }
+
+
+    //    @Override
+//    public String toString() {
+//        long var10000 = this.getID();
+//        return "Libro(ID=" + var10000 + ", titulo=" + this.getTitulo() + ", autor=" + this.getAutor() + ", categoria=" + this.getCategoria() + ", ISBN=" + this.getISBN() + ", precio= $ " + this.getPrecio() + ", stock=" + this.getStock() + ")";
+//    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,45 @@
 package main;
 
-
 import biblioteca.Libro;
 import usuario.Administrador;
 import usuario.Empleado;
 import usuario.Cliente;
 
-
 public class Main {
 
     public static void main(String[] args) {
-
-
         /**
          * testeo inicial sobre usuarios y campos.
          */
         System.out.println("\n" + "testeo inicial sobre usuarios y campos. ##################################################################" + "\n");
 
 
-
-        Administrador administrador1 = new Administrador(1, "43234111", "Sebastian", "Compagnucci", "secomp@gmail.com", "pass");
+            Administrador administrador1 = new Administrador(
+                1,
+                "43234111",
+                "Sebastian",
+                "Compagnucci",
+                "secomp@gmail.com",
+                "pass");
         System.out.println("PRUEBA ADMIN #1: " + administrador1.toString());
 
-        Empleado empleado1 = new Empleado(2, "4444444", "Roberto", "Empleado", "empleado@gmail.com", "pass2");
+            Empleado empleado1 = new Empleado(
+                2,
+                "4444444",
+                "Roberto",
+                "Empleado",
+                "empleado@gmail.com",
+                "pass2");
         System.out.println("PRUEBA EMPLEADO #1: " + empleado1.toString());
 
-        Cliente cliente1 = new Cliente(3, "00000000", "Juan", "Cliente", "cliente@gmail.com", "PASS3");
+            Cliente cliente1 = new Cliente(
+                3,
+                "00000000",
+                "Juan",
+                "Cliente",
+                "cliente@gmail.com",
+                "PASS3");
         System.out.println("PRUEBA CLIENTE #1: " + cliente1.toString());
-
 
         /**
          * testeo inicial sobre libros y campos
@@ -35,9 +47,15 @@ public class Main {
 
         System.out.println("testeo inicial sobre libros y campos. ##################################################################" + "\n");
 
-        Libro libro1 = new Libro(1, "LibroFicticio1", "SebaComp", "CatFalsa", "10#E20", 2000.00, 20);
-
-        System.out.println(libro1.toString());
+            Libro libro1 = new Libro(
+                1,
+                "LibroFicticio1",
+                "SebaComp",
+                "CatFalsa",
+                "10#E20",
+                2000.00,
+                20);
+        System.out.println("PRUEBA LIBRO #1: " + cliente1.toString());
 
     }
 }
